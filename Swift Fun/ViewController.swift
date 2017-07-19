dcc424f8c62ead00a8e4236bbdd7c315e4ac19cd
Steve Kite <skite1@hotmail.com>
2017-07-19 05:51:25 -0700

    First Commit

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,10 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var buttonCount = 0
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var HitMeBtn: UIButton!
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        buttonCount = buttonCount + 1
+        if buttonCount >= 5 {
+            self.HitMeBtn.isHidden = true
+            view.backgroundColor = UIColor.red
+            myLabel.text = "You got me!☠️"
+        
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+
+        
     }
 
     override func didReceiveMemoryWarning() {
