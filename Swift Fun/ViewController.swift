c3d5ebb1ed0ad81a8d7dd810e72edb403c299424
Steve Kite <skite1@hotmail.com>
2017-07-19 07:25:20 -0700

    Now you can add or subtract!

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,23 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var buttonCount = 0
+//    var buttonCount = 0
     
     @IBOutlet weak var myLabel: UILabel!
     @IBOutlet weak var HitMeBtn: UIButton!
     
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var additionSwitch: UISwitch!
     @IBAction func buttonTapped(_ sender: Any) {
-        buttonCount = buttonCount + 1
-        if buttonCount >= 5 {
-            self.HitMeBtn.isHidden = true
-            view.backgroundColor = UIColor.red
-            myLabel.text = "You got me!☠️"
+        let addition = additionSwitch.isOn
         
+        if addition {
+            let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+            
+        }else{
+            let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)" 
         }
+        
+        
+//        buttonCount = buttonCount + 1
+//        if buttonCount >= 5 {
+//            self.HitMeBtn.isHidden = true
+//            view.backgroundColor = UIColor.red
+//            myLabel.text = "You got me!☠️"
+        
+//        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.green
+//        view.backgroundColor = UIColor.green
         
 
         
